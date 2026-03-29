45fd3fd4e2f6e8719f74bdded6a49faa7c3c888e
Martintin1024 <thefraok3010@proton.me>
2026-03-29 14:06:36 -0300

    Add gender and marital status radio groups
    Introduce two GroupBox controls (gpbSexo, gpbEstadoCivil) with RadioButton options for Sexo (Femenino, Masculino) and Estado Civil (Casado, Soltero). Set default checked options, fonts, positions and sizes, and add these controls to the form. Adjust form scaling and client size (AutoScaleDimensions and ClientSize), add margin, and include Suspend/ResumeLayout and PerformLayout calls. Also declare the new control fields at the class level.

--- TP1_GRUPO_6/Form4.Designer.cs
+++ TP1_GRUPO_6/Form4.Designer.cs
@@ -28,21 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gpbSexo = new System.Windows.Forms.GroupBox();
+            this.rdbFemenino = new System.Windows.Forms.RadioButton();
+            this.rdbMasculino = new System.Windows.Forms.RadioButton();
+            this.gpbEstadoCivil = new System.Windows.Forms.GroupBox();
+            this.rdbCasado = new System.Windows.Forms.RadioButton();
+            this.rdbSoltero = new System.Windows.Forms.RadioButton();
+            this.gpbSexo.SuspendLayout();
+            this.gpbEstadoCivil.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gpbSexo
+            // 
+            this.gpbSexo.Controls.Add(this.rdbMasculino);
+            this.gpbSexo.Controls.Add(this.rdbFemenino);
+            this.gpbSexo.Location = new System.Drawing.Point(52, 42);
+            this.gpbSexo.Name = "gpbSexo";
+            this.gpbSexo.Size = new System.Drawing.Size(135, 100);
+            this.gpbSexo.TabIndex = 0;
+            this.gpbSexo.TabStop = false;
+            this.gpbSexo.Text = "Sexo";
+            // 
+            // rdbFemenino
+            // 
+            this.rdbFemenino.AutoSize = true;
+            this.rdbFemenino.Checked = true;
+            this.rdbFemenino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbFemenino.Location = new System.Drawing.Point(17, 31);
+            this.rdbFemenino.Name = "rdbFemenino";
+            this.rdbFemenino.Size = new System.Drawing.Size(85, 20);
+            this.rdbFemenino.TabIndex = 0;
+            this.rdbFemenino.TabStop = true;
+            this.rdbFemenino.Text = "Femenino";
+            this.rdbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // rdbMasculino
+            // 
+            this.rdbMasculino.AutoSize = true;
+            this.rdbMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbMasculino.Location = new System.Drawing.Point(17, 67);
+            this.rdbMasculino.Name = "rdbMasculino";
+            this.rdbMasculino.Size = new System.Drawing.Size(86, 20);
+            this.rdbMasculino.TabIndex = 1;
+            this.rdbMasculino.TabStop = true;
+            this.rdbMasculino.Text = "Masculino";
+            this.rdbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // gpbEstadoCivil
+            // 
+            this.gpbEstadoCivil.Controls.Add(this.rdbSoltero);
+            this.gpbEstadoCivil.Controls.Add(this.rdbCasado);
+            this.gpbEstadoCivil.Location = new System.Drawing.Point(308, 42);
+            this.gpbEstadoCivil.Name = "gpbEstadoCivil";
+            this.gpbEstadoCivil.Size = new System.Drawing.Size(139, 100);
+            this.gpbEstadoCivil.TabIndex = 1;
+            this.gpbEstadoCivil.TabStop = false;
+            this.gpbEstadoCivil.Text = "Estado Civil";
+            // 
+            // rdbCasado
+            // 
+            this.rdbCasado.AutoSize = true;
+            this.rdbCasado.Checked = true;
+            this.rdbCasado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbCasado.Location = new System.Drawing.Point(15, 31);
+            this.rdbCasado.Name = "rdbCasado";
+            this.rdbCasado.Size = new System.Drawing.Size(73, 20);
+            this.rdbCasado.TabIndex = 0;
+            this.rdbCasado.TabStop = true;
+            this.rdbCasado.Text = "Casado";
+            this.rdbCasado.UseVisualStyleBackColor = true;
+            // 
+            // rdbSoltero
+            // 
+            this.rdbSoltero.AutoSize = true;
+            this.rdbSoltero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbSoltero.Location = new System.Drawing.Point(15, 67);
+            this.rdbSoltero.Name = "rdbSoltero";
+            this.rdbSoltero.Size = new System.Drawing.Size(68, 20);
+            this.rdbSoltero.TabIndex = 1;
+            this.rdbSoltero.Text = "Soltero";
+            this.rdbSoltero.UseVisualStyleBackColor = true;
             // 
             // Ejercicio3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(500, 398);
+            this.Controls.Add(this.gpbEstadoCivil);
+            this.Controls.Add(this.gpbSexo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Ejercicio3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio3";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ejercicio3_FormClosed);
+            this.gpbSexo.ResumeLayout(false);
+            this.gpbSexo.PerformLayout();
+            this.gpbEstadoCivil.ResumeLayout(false);
+            this.gpbEstadoCivil.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gpbSexo;
+        private System.Windows.Forms.RadioButton rdbMasculino;
+        private System.Windows.Forms.RadioButton rdbFemenino;
+        private System.Windows.Forms.GroupBox gpbEstadoCivil;
+        private System.Windows.Forms.RadioButton rdbSoltero;
+        private System.Windows.Forms.RadioButton rdbCasado;
     }
 }
